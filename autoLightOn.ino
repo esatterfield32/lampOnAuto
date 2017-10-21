d4d71cdb5d986f8e58a9927d67d3e1814af1c393
esatterfield32 <esatterfield32@gmail.com>
2017-10-21 19:36:00 -0400

    Add pullChain method. bug fix.

--- autoLightOn.ino
+++ autoLightOn.ino
@@ -2,39 +2,37 @@
 int servoPin = 9;
 Servo spool;
 int pos_servo = 180;
-int i;
-
+int i=0;
+int servoDelay = 10;
 void setup() {
   // put your setup code here, to run once:
-spool.attach(servoPin);
-i = 0;
 pos_servo = 0;
 Serial.begin(9600);
+
 }
 
 void loop() {
-if ( i == 0){
-for(pos_servo >= 0; pos_servo<180;){
-  spool.write(pos_servo);
-  delay(50);
-  pos_servo++;
-  Serial.println(pos_servo);
-    }
-    i=1;
+  if(i==0){
+    pullChain();
+    i = 1;
   }
-  if (i == 1){
-    for(pos_servo<=180; pos_servo > 1;){
-      spool.write(pos_servo);
-      delay(50);
-      pos_servo--;
-      Serial.println(pos_servo);
-    }
-    i=2;
-  }
-else{
-  Serial.println("what the frick");
-  
-}
 }
 
+
+void pullChain(){
+  spool.attach(servoPin);
+  for(pos_servo >= 0; pos_servo<180;){
+    spool.write(pos_servo);
+    delay(servoDelay);
+    pos_servo++;
+    Serial.println(pos_servo);
+    }
+  for(pos_servo<=180; pos_servo > 0;){
+    spool.write(pos_servo);
+    delay(servoDelay);
+    pos_servo--;
+    Serial.println(pos_servo);
+    }
+  spool.detach();
+}//end pullChain
 
