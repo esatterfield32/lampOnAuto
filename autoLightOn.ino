357d6bbba68383d7f16ab42fc36b2de15cfd39bf
esatterfield32 <esatterfield32@gmail.com>
2017-10-21 22:16:28 -0400

    Add user input.

--- autoLightOn.ino
+++ autoLightOn.ino
@@ -1,23 +1,29 @@
 #include <Servo.h>
 int servoPin = 9;
 Servo spool;
+int buttonPin = 8;
 int pos_servo = 180;
 int i=0;
 int servoDelay = 10;
+bool buttonState = false;
+bool newButtonState;
+
 void setup() {
-  // put your setup code here, to run once:
 pos_servo = 0;
 Serial.begin(9600);
+pinMode(buttonPin, INPUT_PULLUP);
 
 }
 
 void loop() {
-  if(i==0){
-    pullChain();
-    i = 1;
+ Serial.println(digitalRead(buttonPin));
+ if (digitalRead(buttonPin) == 0){
+  Serial.println("got here");
+  Serial.println(digitalRead(buttonPin));
+  pullChain();
   }
-}
 
+}
 
 void pullChain(){
   spool.attach(servoPin);
@@ -25,13 +31,13 @@ void pullChain(){
     spool.write(pos_servo);
     delay(servoDelay);
     pos_servo++;
-    Serial.println(pos_servo);
+   // Serial.println(pos_servo);
     }
   for(pos_servo<=180; pos_servo > 0;){
     spool.write(pos_servo);
     delay(servoDelay);
     pos_servo--;
-    Serial.println(pos_servo);
+  //  Serial.println(pos_servo);
     }
   spool.detach();
 }//end pullChain
