d50ed0dd51920e2e2d4c8094c360b299205a905d
esatterfield32 <esatterfield32@gmail.com>
2017-10-29 14:43:44 -0400

    Remove servo. Change pins.

--- autoLightOn.ino
+++ autoLightOn.ino
@@ -1,20 +1,11 @@
-#include <Servo.h>
-
-int servoPin = 9;
-Servo spool;
-int buttonPin = 8;
-int pos_servo = 180;
-int i=0;
-int servoDelay = 10;
 int relayPin = 4;
-int pullSwitchPin = 7;
+int pullSwitchPin = 2;
 int incomingByte = 0;
 bool state = false;
 
 void setup() {
 pos_servo = 0;
 Serial.begin(9600);
-pinMode(buttonPin, INPUT_PULLUP);
 pinMode(relayPin, OUTPUT);
 pinMode(pullSwitchPin, INPUT_PULLUP);
 digitalWrite(relayPin, HIGH);
@@ -34,30 +25,8 @@ void loop() {
    }
    
  }
-    
-  
-
 
 }
-
-
-void pullChain(){
-  spool.attach(servoPin);
-  for(pos_servo >= 0; pos_servo<180;){
-    spool.write(pos_servo);
-    delay(servoDelay);
-    pos_servo++;
-   // Serial.println(pos_servo);
-    }
-  for(pos_servo<=180; pos_servo > 0;){
-    spool.write(pos_servo);
-    delay(servoDelay);
-    pos_servo--;
-  //  Serial.println(pos_servo);
-    }
-  spool.detach();
-}//end pullChain
-
 
 
 void activateRelay(){
